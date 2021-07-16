af4193b3abddd2e8a76cf1d8ad4401b4aa42cd86
Logvina <irinlogvina9@gmail.com>
2021-07-16 13:32:16 +0300

    template, API, html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+
+import PhotoApi from './js/API';
+import imageTpl from './template/photo-card.hbs';
+
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
---
+++ src/js/API.js
@@ -0,0 +1,37 @@
+const API_KEY = '22412207-a0ef56cb918658d27ea3da2a0';
+const BASE_URL = 'https://pixabay.com/api';
+
+
+export default class ApiPhoto {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchPhoto() {
+        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(({ hits }) => {
+            this.incrementPage();
+            return hits;
+        });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
